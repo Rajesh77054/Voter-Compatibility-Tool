a54278e10e37f5c96ed8f4e3509f5c8f03b0fbee
Rajesh77054 <35733477+Rajesh77054@users.noreply.github.com>
2024-10-23 22:45:09 -0500

    Update and rename FrontendCode(index.js with React) to frontend/src/index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css'; // Include your CSS
+import AdminPage from './pages/AdminPage';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <AdminPage />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
