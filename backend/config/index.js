3e9e811872804bc9e9f857641bd633562a1f30f2
Rajesh77054 <35733477+Rajesh77054@users.noreply.github.com>
2024-10-28 06:28:38 -0500

    Update index.js

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,1 +1,11 @@
+require('dotenv').config();
+const development = require('./development');
+const production = require('./production');
 
+const config = {
+  development,
+  production,
+};
+
+const env = process.env.NODE_ENV || 'development';
+module.exports = config[env];
